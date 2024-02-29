2a41cf570003730bf9c635a7c0402bcf3b498465
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-02-29 15:19:56 -0500

    Add 3d container

--- ProjetVR/Assets/Game/Scripts/Components/Container.cs
+++ ProjetVR/Assets/Game/Scripts/Components/Container.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Container;
 
+[ExecuteInEditMode]
 public class Container : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum AlignPosition
     {
-        
+        LEFT, CENTER, RIGHT
     }
 
-    // Update is called once per frame
-    void Update()
+    public float space;
+    public AlignPosition alignPosition;
+
+#if UNITY_EDITOR
+    private void Update()
     {
-        
+        UpdateChildsPosition();
+    }
+#endif
+
+    private void UpdateChildsPosition()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+
+            switch (alignPosition)
+            {
+                case AlignPosition.LEFT:
+                    child.localPosition = new Vector3(space, 0, 0) * i;
+                    break;
+                case AlignPosition.CENTER:
+                    child.localPosition = (new Vector3(space, 0, 0) * i) - new Vector3(space * (transform.childCount - 1), 0, 0) / 2f;
+                    break;
+                case AlignPosition.RIGHT:
+                    child.localPosition = new Vector3(-space, 0, 0) * i;
+                    break;
+            }
+        }
     }
 }

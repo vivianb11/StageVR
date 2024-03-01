2b5b9f963ef039862c33939e1694230b9921f82f
RedMenUSA <martin.bechu@gmail.com>
2024-03-01 17:47:37 -0500

    enemies v1

--- ProjetVR/Assets/Game/Scripts/Mob.cs
+++ ProjetVR/Assets/Game/Scripts/Mob.cs
@@ -2,16 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+
 public class Mob : MonoBehaviour
 {
-    public Transform target; 
-
+    public Transform target;
+    public int LifePoints = 1;
     public float moveSpeed = 5f;
+    public bool EnemyPush = false;
+    public float pushForce = 10f;
+    private bool _isKnocked = false;
+    Vector3 spawnDirection;
 
+    void Start()
+    {
+        spawnDirection = transform.position;
+    }
     void Update()
     {
         transform.LookAt(target.position);
 
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (!_isKnocked)
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (LifePoints == 0)
+            Destroy(this.gameObject);
     }
+
+    private void OnTriggerEnter(Collider shield)
+    {
+        if (shield.gameObject.CompareTag("Shield"))
+        {
+            LifePoints -= 1;
+
+            if (EnemyPush)
+            {
+                StartCoroutine(Knocked());
+            }
+        }
+    }
+    private IEnumerator Knocked()
+    {
+        _isKnocked = true;
+        Rigidbody enemyRigidbody = GetComponent<Rigidbody>();
+        if (enemyRigidbody != null)
+        {
+            enemyRigidbody.AddForce(spawnDirection * pushForce, ForceMode.Impulse);
+        }
+        yield return new WaitForSeconds(2f);
+        _isKnocked = false;
+
+    }
+
 }

3af159fad3c9f1825985452cff8f06afe12b8ffe
vivianb11 <vivianb.info@gmail.com>
2024-03-14 19:46:33 -0400

    Added Calibration V0.1

---
+++ ProjetVR/Assets/Game/Datas/EyeTrackingData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeTrackingData : MonoBehaviour
+{
+    public static Vector3 eyeOffset = Vector3.zero;
+}

618dd90c1c5aba0f0d93aca91c6c16b7f9d113e6
Alexander <78765584+Z4T1NA@users.noreply.github.com>
2024-03-26 14:27:14 -0400

    new shield script

---
+++ ProjetVR/Assets/Game/Scripts/Shield Detection 2.cs
@@ -0,0 +1,81 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+public class ShieldDetection2 : MonoBehaviour
+{
+    [SerializeField] [ReadOnly] private GameObject currentActiveShield;
+
+    [SerializeField] float minDetectionRange = 1.0f;
+
+    [SerializeField] Transform[] points;
+
+    void Start()
+    {
+        currentActiveShield = null;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 hitPosition = EyeManager.Instance.hitPosition;
+        float distanceToHitpoint = Vector3.Distance(transform.position, hitPosition);
+
+        if (distanceToHitpoint < minDetectionRange)
+            return;
+
+        Transform closestPoint = points[0];
+        float closestDistance = Vector3.Distance(closestPoint.position, hitPosition);
+
+        foreach (Transform t in points)
+        {
+            float distance = Vector3.Distance(t.position, hitPosition);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestPoint = t;
+            }
+        }
+        // Here
+        Activate(closestPoint);
+    }
+
+    public void changeDirection(GameObject obj)
+    {
+        DeactivateCurrentShield();
+
+        obj.SetActive(true);
+
+        currentActiveShield = obj;
+    }
+
+
+    // Here
+    public void Activate(GameObject obj)
+    {
+        DeactivateCurrentShield();
+
+        obj.SetActive(true);
+
+        currentActiveShield = obj;
+    }
+    // replace old shield with 
+
+    // Here
+    public void Activate(Transform obj)
+    {
+        DeactivateCurrentShield();
+
+        obj.gameObject.SetActive(true);
+
+        currentActiveShield = obj.gameObject;
+    }
+
+    // Here
+    private void DeactivateCurrentShield()
+    {
+        if (currentActiveShield != null && currentActiveShield.activeSelf)
+        {
+            currentActiveShield.SetActive(false);
+        }
+    }
+}

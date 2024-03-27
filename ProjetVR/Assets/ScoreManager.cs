3cb037af5d855720d049def52829b7e737e71ad2
ThomasOuellet <thomas.ouellet25@gmail.com>
2024-03-27 16:59:00 -0400

    commencer scale up text score

--- ProjetVR/Assets/ScoreManager.cs
+++ ProjetVR/Assets/ScoreManager.cs
@@ -15,15 +15,70 @@ public class ScoreManager : Singleton<ScoreManager>
     [Header("Target")]
     [SerializeField] TextMeshPro scoreDisplay;
 
-    
+
+    //variables for scale up/down de Thomas
+    public float minValue = 6f;
+    public float maxValue = 9f;
+    public float duration = 1f;
+
+    private float currentValue;
+    private bool increasing = true;
+    private float timer = 0f;
+    private bool interpolationComplete = false;
+    private bool scaleUp = false;
+
+
     private void Start()
     {
+        currentValue = minValue;
         if (PlayerPrefs.HasKey("HighScore")) bestScore = PlayerPrefs.GetInt("HighScore");
+    }
+    
+    //scale up de thom, à essayer avec le casque
+    void ScaleUpScaleDown()
+    {
+        if (scaleUp == true)
+        {
+            if (interpolationComplete)
+                return;
+
+            timer += Time.deltaTime;
+
+            if (timer >= duration)
+            {
+                timer = 0f;
+                increasing = !increasing;
+                if (!increasing)
+                    interpolationComplete = true; // Stop updating after reaching maximum value
+            }
+
+            if (!interpolationComplete)
+            {
+                if (increasing)
+                {
+                    currentValue = Mathf.Lerp(minValue, maxValue, timer / duration);
+                }
+                else
+                {
+                    currentValue = Mathf.Lerp(maxValue, minValue, timer / duration);
+                }
+            }
+            // Use the currentValue for whatever purpose you need
+            Debug.Log("Current value: " + currentValue);
+            scaleUp = false;
+        }
+    }
+
+
+    void Update()
+    {
+        ScaleUpScaleDown();
     }
 
     public void AddScore(int scoreToAdd)
     {
         currentScore += scoreToAdd;
+        scaleUp = true;
         scoreDisplay.text = currentScore.ToString();
     }
 

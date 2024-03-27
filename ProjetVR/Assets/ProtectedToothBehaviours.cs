69966419fefb2dd93ed382c41c45105599e0f600
ThomasOuellet <thomas.ouellet25@gmail.com>
2024-03-27 14:37:04 -0400

    Added shake and tooth explosion

--- ProjetVR/Assets/ProtectedToothBehaviours.cs
+++ ProjetVR/Assets/ProtectedToothBehaviours.cs
@@ -14,6 +14,20 @@ public class ProtectedToothBehaviours : MonoBehaviour
     [SerializeField] Material material2HP; //the material the tooth has at 2 hp
     [SerializeField] Material material1HP; //the material the tooth has at 1 hp
 
+    [SerializeField] float shakeMagnitude = 0.1f;
+    [SerializeField] float shakeSpeed = 50f;
+    [SerializeField] float shakeDuration = 3f;
+
+    private Vector3 originalPosition;
+
+    public GameObject toothExplosion;
+
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
     public void Damaged()
     {
         if (health <= 0) return;
@@ -37,13 +51,31 @@ public class ProtectedToothBehaviours : MonoBehaviour
         }
     }
 
+    IEnumerator ShakeAndDie()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < shakeDuration)
+        {
+            Vector3 shakeOffset = Random.insideUnitSphere * shakeMagnitude;
+            transform.position = originalPosition + shakeOffset;
+            elapsedTime += Time.deltaTime * shakeSpeed;
+            yield return null;
+        }
+
+        Instantiate(toothExplosion); //the tooth explodes into many pieces
+        Destroy(gameObject);
+
+        yield return null;
+    }
+
     private bool IsDeadCheck()
     {
         bool condition = health == 0;
         if (condition)
         {
             onDeath.Invoke();
-            Destroy(gameObject);
+            StartCoroutine(ShakeAndDie());
         }
         return condition;
     }

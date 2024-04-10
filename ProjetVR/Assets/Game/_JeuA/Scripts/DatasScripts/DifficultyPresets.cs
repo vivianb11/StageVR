0733850f7b2165ee2b124089342c75121c64c957
JossQrn <josselin.qrn@gmail.com>
2024-04-10 10:08:57 -0400

    Editable Mob Speed for Difficulty Parameters

--- ProjetVR/Assets/Game/_JeuA/Scripts/DatasScripts/DifficultyPresets.cs
+++ ProjetVR/Assets/Game/_JeuA/Scripts/DatasScripts/DifficultyPresets.cs
@@ -8,6 +8,7 @@ public class DifficultyPresets : ScriptableObject
     [Header("Difficulty Parameters")]
     public int countCondition;
     public float spawnInterval;
+    public float mobSpeed;
     [Range(1, 8)] public int numberSpawner;
     public int weightEnemy1;
     public int weightEnemy2;

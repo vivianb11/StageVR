223d728e723d9f5358f0842c5af1b3c3ae65e8d9
ThomasOuellet <thomas.ouellet25@gmail.com>
2024-04-23 10:28:57 -0400

    commencer text niveaux

--- ProjetVR/Assets/PulsatingText.cs
+++ ProjetVR/Assets/PulsatingText.cs
@@ -10,15 +10,24 @@ public class PulsatingText : MonoBehaviour
     public float minValueFontSize = 6f;
     public float maxValueFontSize = 12f;
     public float duration = 1f;
+    public float durationColor = 0.1f;
 
     private float FontSizeValue; //the variable that constantly changes up and down
     private bool increasing = true;
+    private bool increasingColor = true;
     private float timer = 0f;
+    private float timerColor = 0f;
+    public bool colorActivated;
 
 
     private void Update()
     {
         FontSizePulsating();
+
+        if (colorActivated)
+        {
+            ColorPulsating();
+        }
     }
 
     public void FontSizePulsating()
@@ -41,5 +50,25 @@ public class PulsatingText : MonoBehaviour
         }
 
         gameObject.GetComponent<TextMesh>().characterSize = FontSizeValue;
+    }
+
+    public void ColorPulsating()
+    {
+        timerColor += Time.deltaTime;
+
+        if (timerColor >= durationColor)
+        {
+            timerColor = 0f;
+            increasingColor = !increasingColor;
+        }
+
+        if (increasingColor)
+        {
+            gameObject.GetComponent<TextMesh>().color = Color.yellow;
+        }
+        else
+        {
+            gameObject.GetComponent<TextMesh>().color = Color.white;
+        }
     }
 }

a943abae89702b0c9a4ff3e4435692dd17749e31
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-04-28 15:26:48 -0400

    Fix shield nump direction

--- ProjetVR/Assets/Game/_JeuB/Scripts/Shield.cs
+++ ProjetVR/Assets/Game/_JeuB/Scripts/Shield.cs
@@ -9,16 +9,12 @@ namespace JeuB
         public float moveSpeed = 5f;
         public float returnSpeed = 2f;
 
-        private bool isTweening = false;
         public float moveDuration = 0.5f;
 
         public UnityEvent OnShieldHit;
 
         private Tween tweener;
         private ProtectedToothBehaviours tooth;
-
-        //l'idée a joss et thomas a renomé la fonction (le gros du travail)
-        public void TweenerChecker(bool tweening) => isTweening = tweening;
 
         private void Awake()
         {
@@ -33,12 +29,7 @@ namespace JeuB
                 tooth.enemyPoints = other.GetComponent<Mob>().scoreOnDeath;
                 OnShieldHit.Invoke();
 
-                if (!isTweening)
-                {
-                    tweener.tweenMontages[0].tweenProperties[1].to = gameObject.transform.localPosition;
-                    tweener.tweenMontages[0].tweenProperties[0].to = new Vector3(gameObject.transform.localPosition.x / 0.75f, gameObject.transform.localPosition.y / 0.75f, gameObject.transform.localPosition.z / 0.75f);
-                    tweener.PlayMontages();
-                }
+                tweener.PlayMontages();
             }
         }
     }

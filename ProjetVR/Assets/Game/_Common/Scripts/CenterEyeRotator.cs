a494812f06e8c26553a6048634ddf14103016743
vivianb11 <vivianb.info@gmail.com>
2024-05-04 17:25:54 -0400

    Add of more complex offset correction

--- ProjetVR/Assets/Game/_Common/Scripts/CenterEyeRotator.cs
+++ ProjetVR/Assets/Game/_Common/Scripts/CenterEyeRotator.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class CenterEyeRotator : MonoBehaviour
@@ -5,13 +6,31 @@ public class CenterEyeRotator : MonoBehaviour
     [SerializeField] Transform leftEye;
     [SerializeField] Transform rightEye;
 
+    [SerializeField] bool useSimpleOffset = true;
+
+    [ShowIf("useSimpleOffset")]
     [SerializeField] Vector2 eyeOffset = new Vector2(0, 0);
+
+    [HideIf("useSimpleOffset")]
+    [SerializeField] Vector2 LeftOffset = new Vector2(0, 0);
+    [HideIf("useSimpleOffset")]
+    [SerializeField] Vector2 RightOffset = new Vector2(0, 0);
 
     private void FixedUpdate()
     {
         transform.rotation = Quaternion.Lerp(leftEye.rotation, rightEye.rotation, 0.5f);
 
-        // Apply eye offset to the rotation
-        transform.rotation = Quaternion.Euler(eyeOffset) * transform.rotation;
+        if (useSimpleOffset)
+        {
+            // Apply eye offset to the rotation
+            transform.rotation = Quaternion.Euler(eyeOffset) * transform.rotation;
+        }
+        else
+        {
+            // looks at the left eye rotation on the y axis and interpolates 0 if at -75 degrees and 1 if at 75 degrees
+            float lerpValue = Mathf.InverseLerp(-75, 75, leftEye.localEulerAngles.y);
+            // Apply the offset to the rotation
+            transform.rotation = Quaternion.Euler(Vector2.Lerp(LeftOffset, RightOffset, lerpValue)) * transform.rotation;
+        }
     }
 }

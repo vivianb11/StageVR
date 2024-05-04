388a64cc1bb985c9404b8aef70e50fd1ed9785df
vivianb11 <vivianb.info@gmail.com>
2024-05-04 15:55:35 -0400

    Removed Blink unused Code

--- ProjetVR/Assets/Game/_Common/Scripts/Managers/EyeManager.cs
+++ ProjetVR/Assets/Game/_Common/Scripts/Managers/EyeManager.cs
@@ -15,26 +15,6 @@ public class EyeManager : MonoBehaviour
     public float maxValue;
     public float value;
 
-    #region Eyes
-    public enum EyeState
-    {
-        OPENED, CLOSED
-    }
-
-    [Header("Eyes Datas")]
-    public float leftEyeOpeningPercent = 1f;
-    public float rightEyeOpeningPercent = 1f;
-    [SerializeField]
-    private float closedTime;
-    [SerializeField]
-    private float blinkBuffer = 1;
-
-    [SerializeField]
-    private EyeState leftEyeState = EyeState.OPENED;
-    [SerializeField]
-    private EyeState rightEyeState = EyeState.OPENED;
-    #endregion
-
     public Interactable interactable { get; private set; }
 
     [Header("Grab")]
@@ -72,40 +52,6 @@ public class EyeManager : MonoBehaviour
 
     private void Update()
     {
-        #region Debug
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            leftEyeOpeningPercent = 1;
-            rightEyeOpeningPercent = 1;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            leftEyeOpeningPercent = 0;
-            rightEyeOpeningPercent = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (leftEyeOpeningPercent == 0)
-            {
-                leftEyeOpeningPercent = 1;
-                rightEyeOpeningPercent = 1;
-            }
-            else
-            {
-                leftEyeOpeningPercent = 0;
-                rightEyeOpeningPercent = 0;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            leftEyeOpeningPercent = Mathf.Clamp(leftEyeOpeningPercent + Input.mouseScrollDelta.y * 0.1f, 0f, 1f);
-        if (Input.GetKey(KeyCode.RightArrow))
-            rightEyeOpeningPercent = Mathf.Clamp(rightEyeOpeningPercent + Input.mouseScrollDelta.y * 0.1f, 0f, 1f);
-        #endregion
-
-        HandleEyesStats();
-
         RaycastInteractable();
     }
 
@@ -126,61 +72,6 @@ public class EyeManager : MonoBehaviour
 
         }
     }
-
-    #region Eyes Methods
-    private IEnumerator BlinkBuffer(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        if (leftEyeOpeningPercent > 0 && rightEyeOpeningPercent > 0)
-            blink?.Invoke();
-    }
-
-    private void HandleEyesStats()
-    {
-        //Left Eye
-        if (leftEyeOpeningPercent == 0f && leftEyeState == EyeState.OPENED)
-        {
-            leftEyeState = EyeState.CLOSED;
-            leftEyeClosed?.Invoke();
-        }
-        else if (leftEyeOpeningPercent > 0f && leftEyeState == EyeState.CLOSED)
-        {
-            leftEyeState = EyeState.OPENED;
-            leftEyeOpened?.Invoke();
-            closedTime = 0f;
-        }
-
-        //Right Eye
-        if (rightEyeOpeningPercent == 0f && rightEyeState == EyeState.OPENED)
-        {
-            rightEyeState = EyeState.CLOSED;
-            rightEyeClosed?.Invoke();
-        }
-        else if (rightEyeOpeningPercent > 0f && rightEyeState == EyeState.CLOSED)
-        {
-            rightEyeState = EyeState.OPENED;
-            rightEyeOpened?.Invoke();
-            closedTime = 0f;
-        }
-
-        if (leftEyeState == EyeState.CLOSED && rightEyeState == EyeState.CLOSED)
-        {
-            closedTime += Time.deltaTime;
-        }
-
-        if (rightEyeState == EyeState.OPENED || rightEyeState == EyeState.OPENED)
-        {
-            preBlink = false;
-        }
-
-        if (!preBlink && leftEyeOpeningPercent == 0 && rightEyeOpeningPercent == 0)
-        {
-            preBlink = true;
-            StartCoroutine(BlinkBuffer(blinkBuffer));
-        }
-    }
-    #endregion
 
     private void OnInteractableSelected()
     {

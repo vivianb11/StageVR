260c6e21cb5f2c776bfe24d470366a646cfaf487
JossQrn <josselin.qrn@gmail.com>
2024-05-10 09:57:47 -0400

    Bonus Mob Script from abstract class

---
+++ ProjetVR/Assets/Game/_JeuB/Scripts/Entities/BonusMob.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JeuB {
+    public class BonusMob : Bonus
+    {
+        [SerializeField] int scoreToAdd;
+
+        protected override void EntityStart() {}
+        
+
+        protected override void EntityUpdate() {}
+
+        public override void ApplyBonus(ProtectedToothBehaviours tooth)
+        {
+            tooth.enemyPoints = scoreToAdd;
+            tooth.ScoreMultiplier();
+        }
+
+    }
+}

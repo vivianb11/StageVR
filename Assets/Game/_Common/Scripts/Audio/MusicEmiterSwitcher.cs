9885d855d6de6e596f57bedd67984ce5151a7026
Vivian <vivianb.info@gmail.com>
2024-06-05 15:42:35 -0400

    Fixed music

--- Assets/Game/_Common/Scripts/Audio/MusicEmiterSwitcher.cs
+++ Assets/Game/_Common/Scripts/Audio/MusicEmiterSwitcher.cs
@@ -1,5 +1,6 @@
 using SignalSystem;
 using System;
+using System.Linq;
 using UnityEngine;
 
 public class MusicEmiterSwitcher : MonoBehaviour
@@ -10,15 +11,24 @@ public class MusicEmiterSwitcher : MonoBehaviour
     [Space(10)]
     public MusicOnSignal[] musicOnSignals;
 
+    public bool SkinCheck, ModeCheck;
+
     public void Start()
     {
         SignalManager.Instance.signalCalled.AddListener(OnSignalReceived);
 
         if (musicEmiter == null) if (TryGetComponent(out musicEmiter)) Debug.Log("No Music Emiter Assigned or On Gameobject");
+
+        if (SkinCheck) OnSignalReceived(JeuB.JeuBCommands.lastSkinSignal.name);
+        if (ModeCheck) OnSignalReceived(JeuB.JeuBCommands.lastModeSignal.name);
     }
 
     protected virtual void OnSignalReceived(string value)
     {
+        if (musicOnSignals.Where(x => x.signal.name == value).Count() < 1) value = JeuB.JeuBCommands.lastSkinSignal.name;
+
+        if (JeuB.JeuBCommands.lastModeSignal.name == "SophoriqueMusic") return;
+
         foreach (MusicOnSignal musicOnSignal in musicOnSignals)
         {
             if (musicOnSignal.signal.name == value)
